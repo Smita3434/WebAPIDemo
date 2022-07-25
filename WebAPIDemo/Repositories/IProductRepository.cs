95702a5fdb798f5048b45f67d7e9737c0a589e56
Smita3434 <sayalig94@gmail.com>
2022-07-25 13:15:47 +0530

    Add project files.

---
+++ WebAPIDemo/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Model;
+
+namespace WebAPIDemo.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        int AddProduct(Product prod);
+        int ModifyProduct(Product prod);
+        int DeleteProduct(int id);
+    }
+}
---
+++ WebAPIDemo/Services/ProductServices.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Model;
+using WebAPIDemo.Repositories;
+
+namespace WebAPIDemo.Services
+{
+    public class ProductServices : IProductServices
+    {
+        private readonly IProductRepository _iProductRepo;
+
+        public ProductServices(IProductRepository iProductRepo)
+        {
+            _iProductRepo = iProductRepo;
+              
+        }
+
+        public int AddProduct(Product prod)
+        {
+            return _iProductRepo.AddProduct(prod);
+        }
+
+        public int DeleteProduct(int id)
+        {
+            return _iProductRepo.DeleteProduct(id);
+
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _iProductRepo.GetAllProducts();
+            
+
+        }
+
+        public Product GetProductById(int id)
+        {
+            var product = _iProductRepo.GetProductById(id);
+            return product;
+        }
+
+            public int ModifyProduct(Product prod)
+        {
+            return _iProductRepo.ModifyProduct(prod);
+        }
+    }
+}
